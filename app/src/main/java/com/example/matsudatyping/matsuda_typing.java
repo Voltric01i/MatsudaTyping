a636d50142681765f899bfa227af1727f67d6f8d
Voltric01i <yupam0922@gmail.com>
2019-02-28 03:35:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/matsudatyping/matsuda_typing.java
@@ -0,0 +1,23 @@
+package com.example.matsudatyping;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class matsuda_typing extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matsuda_typing);
+
+    }
+
+
+
+
+}
